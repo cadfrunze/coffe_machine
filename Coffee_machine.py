d9c2c3e5770682ce578b41bea50c6c410ba86725
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-07-14 23:05:34 +0300

    Update Coffee_machine.py

--- Coffee_machine.py
+++ Coffee_machine.py
@@ -98,7 +98,7 @@ def monezi_cafea(money):
                 cerere = int(input(f"How many {i}: "))
                 money = money + (cerere * monede[i])
                 if money >= cafeaua['cost']:
-                    print(f"Ai introdus {money}....Cafeaua se prepara")
+                    print(f"Ai introdus {money}....Cafeaua se prepare")
                     break
                 else:
                     print(f"Pana acum ai introdus {money}")
